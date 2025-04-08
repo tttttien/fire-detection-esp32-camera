3d5042118848ef07cbdfc9c32dde92a9eb276aa3
Ba Dac <lebadac@Bas-MacBook-Pro.local>
2025-04-08 23:40:56 +0700

    Change color for nav & background + change style for nav of esp32cam

--- app/src/main/java/com/example/camera/Esp32CamActivity.kt
+++ app/src/main/java/com/example/camera/Esp32CamActivity.kt
@@ -43,7 +43,7 @@ class Esp32CamActivity : ComponentActivity() {
 
         setupWebView()
 
-        webView.loadUrl("http://192.168.51.126/stream") // Update the ESP32 stream URL
+        webView.loadUrl("http://192.168.1.64/stream") // Update the ESP32 stream URL
 
         handler.postDelayed(frameCaptureRunnable, frameCaptureInterval)
     }
